f8add82cc6ab58bcfcb00720c275bcc820b43d37
shendyppy <shendyppy@gmail.com>
2021-08-27 03:22:18 +0700

    minor fix

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const UserController = require("../controllers/UserController");
-const authentication = require("../middlewares/auth");
+const { authentication } = require("../middlewares/auth");
 
 router.post("/register", UserController.register);
 router.post("/login", UserController.login);
